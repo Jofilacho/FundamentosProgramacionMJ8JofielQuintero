8479b12978c379b5e4d09b96084f5a0e56cc936d
Jofilacho <jofiel.quinteroo@upb.edu.co>
2026-07-30 08:33:52 -0500

    Primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //aquí va el código fuente
+            //Variables
+            string nombre = "Jofiel";
+            Console.WriteLine("Bienvenido"+ nombre+" al curso de Fundamentos de Programación");
+
+        }
+    }
+}
