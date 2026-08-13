86af1deec125d0539db2f92ce6800ec60577d5bb
Jofilacho <jofiel.quinteroo@upb.edu.co>
2026-08-13 09:46:37 -0500

    CondicionalMultiple

---
+++ 6.CondicionalesMultiples/6.CondicionalesMultiples/6.CondicionalesMultiples.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace _6.CondicionalesMultiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Condicionales Múltiples
+            //Crear un algorimo que muestre por patalla un menú con 5 opciones, y según la opción elegida del menú, muestere un mensaje por pantalla donde se muestre la opción elegida, en caso de que se seleccione una opción que no está en el menú mostrar un mensaje de error.
+            int respuesta = 0;
+            Console.WriteLine("-------------Menú---------------");
+            Console.WriteLine("1. Opción 1     2. Opción 2");
+            Console.WriteLine("3. Opción 3     4. Opción 4");
+            Console.WriteLine("5. Opción 5");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Elija una opción del menú:");
+            respuesta=int.Parse(Console.ReadLine());
+
+            switch (respuesta) 
+            {
+                case 1:
+                    Console.WriteLine("Eligió Opción 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Eligió Opción 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Eligió Opción 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Eligió Opción 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Eligió Opción 5");
+                    break;
+                default:
+                    Console.WriteLine("Elija una opción válida");
+                    break;
+            }
+        }
+    }
+}
