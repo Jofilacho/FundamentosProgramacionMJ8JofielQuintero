48b5f82d950f2cd151d12cb2ea9804213cc940cb
Jofilacho <jofiel.quinteroo@upb.edu.co>
2026-09-10 09:31:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ParcialJofielCiclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int personas = 0;
+            int contador = 0;
+            int ticket = 0;
+            int sala = 0;
+            while (personas<=40 ) 
+            {
+                Console.WriteLine("Ingrese su ticket");
+                int.Parse( Console.ReadLine() );
+                
+                
+            }
+            personas++;
+
+            sala = personas;
+            if ( sala == 40 ) 
+            {
+                Console.WriteLine("¡Sala llena! Fin de la función.");
+            }
+        }
+    }
+}
