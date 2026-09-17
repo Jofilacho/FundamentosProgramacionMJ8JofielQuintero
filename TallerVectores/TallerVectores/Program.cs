576418d1b033dc560e1be84338adec5318def8a5
Jofilacho <jofiel.quinteroo@upb.edu.co>
2026-09-17 09:26:05 -0500

    TallerVectores-ArreglosUnidimensionales

---
+++ TallerVectores/TallerVectores/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace TallerVectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //. Escribir un algoritmo que permita llenar un vector[15] con números enteros, y luego encuentre y muestre el valor máximo y mínimo de los números ingresados.
+            int[] vector = new int[15];
+            Random rnd = new Random();
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = rnd.Next(-50, 51);
+            }
+            int maximo = vector[0];
+            int minimo = vector[0];
+        }   
+    } 
+}
